d44254840267bf7b513ff34902b5bd513e3a9732
sara <vanegasnicoll005@gmail.com>
2025-12-01 09:22:49 -0500

    Actualización GITHUB

--- Proyecto-MEPO/Proyecto-MEPO Este si/supermer.js
+++ Proyecto-MEPO/Proyecto-MEPO Este si/supermer.js
@@ -1,32 +1,4 @@
-const supermercados = [
-    {
-        nombre: "Éxito",
-        logo: "imagenes/exito.png",
-        descripcion: "Tu tienda, tu vida. Siempre con los mejores precios.",
-        direccion: "Cra. 24 # 40-53, Ibagué, Tolima",
-        horario: "Lunes a Sábado: 8:00 AM - 9:00 PM\nDomingo y Festivos: 9:00 AM - 8:00 PM",
-        web: "https://www.exito.com",
-        mapaUrl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3975.5647149396435!2d-75.23176108523302!3d4.43897079669923!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e38e6b7c5b5b5b5%3A0x5b5b5b5b5b5b5b5b!2sExito!5e0!3m2!1ses!2sco!4v1620000000000!5m2!1ses!2sco" // URL de ejemplo
-    },
-    {
-        nombre: "D1",
-        logo: "imagenes/d1.png",
-        descripcion: "Los precios más bajos, siempre.",
-        direccion: "Calle 50 # 4-45, Ibagué, Tolima",
-        horario: "Lunes a Sábado: 8:00 AM - 8:00 PM\nDomingo: 9:00 AM - 6:00 PM",
-        web: "https://www.d1.com.co",
-        mapaUrl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3975.5647149396435!2d-75.23176108523302!3d4.43897079669923!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e38e6b7c5b5b5b5%3A0x5b5b5b5b5b5b5b5b!2sD1!5e0!3m2!1ses!2sco!4v1620000000001!5m2!1ses!2sco" // URL de ejemplo
-    },
-    {
-        nombre: "Surtiplaza",
-        logo: "imagenes/surti.png",
-        descripcion: "La plaza de tu hogar.",
-        direccion: "Cra. 3 # 5-70, Ibagué, Tolima",
-        horario: "Lunes a Sábado: 8:00 AM - 8:00 PM\nDomingo: 9:00 AM - 1:00 PM",
-        web: "https://www.surtiplaza.com.co",
-        mapaUrl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3975.5647149396435!2d-75.23176108523302!3d4.43897079669923!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e38e6b7c5b5b5b5%3A0x5b5b5b5b5b5b5b5b!2sSurtiplaza!5e0!3m2!1ses!2sco!4v1620000000003!5m2!1ses!2sco" // URL de ejemplo
-    }
-];
+
 
 function mostrarSupermercados() {
     const gridContainer = document.getElementById('supermercados-grid');
@@ -63,6 +35,7 @@ function mostrarSupermercados() {
         gridContainer.appendChild(card);
     });
 }
+
 
 // se cargan los supermercados cuando la pagina termine de cargas
 window.onload = mostrarSupermercados;
