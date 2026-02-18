2586795ba4b4ee3e302dfa724f6b37ef8a5cfdfa
Josmanpro <josephalfonsoforero@gmail.com>
2026-02-18 07:15:53 -0500

    Actualización GITHUB

--- PROYECTO/Proyecto-MEPO/public/regis2.php
+++ PROYECTO/Proyecto-MEPO/public/regis2.php
@@ -1,35 +1,39 @@
 <?php
 
+session_start(); // 🔥 Necesario para usar sesiones
+
 $servidor = "localhost";
 $usuario = "root";
 $clave = "";
 $basededatos = "mepo";
 
-
 $enlace = mysqli_connect($servidor,$usuario,$clave,$basededatos);
 
-if(isset($_POST["boton"])){
+if($_SERVER["REQUEST_METHOD"] == "POST"){
 
-$ndocumento = $_POST["docu"];
-$nombre = $_POST["nombre"];
-$apellido = $_POST["apellido"];
-$correo_tel = $_POST["user"];
-$contrasena = sha1($_POST["contrasena"]);
-$rol_id = 2;
+    $ndocumento = $_POST["docu"];
+    $nombre = $_POST["nombre"];
+    $apellido = $_POST["apellido"];
+    $correo_tel = $_POST["user"];
+    $contrasena = sha1($_POST["contrasena"]);
+    $rol_id = 2;
 
+    $insertardatos = "INSERT INTO usuario 
+    (ndocumento, nombre, apellido, correo_tel, contrasena, rol_id) 
+    VALUES 
+    ('$ndocumento','$nombre','$apellido','$correo_tel','$contrasena','$rol_id')";
 
+    $ejecutarInsertar = mysqli_query($enlace,$insertardatos);
 
-$insertardatos= "INSERT INTO usuario (ndocumento, nombre, apellido, correo_tel, contrasena, rol_id)   VALUES ('$ndocumento','$nombre','$apellido','$correo_tel','$contrasena','$rol_id')";
+    if($ejecutarInsertar){
+        // 🔥 Creamos el mensaje en sesión
+        $_SESSION['mensaje_exito'] = "Usuario registrado correctamente";
 
-$ejecutarInsertar = mysqli_query($enlace,$insertardatos);
-
-if($ejecutarInsertar){
-    header("Location: login.php?registro=ok");
-}else{
-    echo "error".mysqli_error($enlace);;
-}
-exit();
-
-
+        // 🔥 Redirigimos al login
+        header("Location: login.php");
+        exit();
+    }else{
+        echo "Error: " . mysqli_error($enlace);
+    }
 }
 ?>
