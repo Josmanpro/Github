cc27959efaef5a6dd6d65e2f2d0e8ccc2ae58c41
sara <vanegasnicoll005@gmail.com>
2026-02-18 10:51:08 -0500

    Actualización GITHUB

--- PROYECTO/Proyecto-MEPO/public/ahorro.php
+++ PROYECTO/Proyecto-MEPO/public/ahorro.php
@@ -1,3 +1,21 @@
+<?php
+session_start();
+require("regis2.php"); // conexión a la base de datos
+
+$usuario = null;
+
+if (isset($_SESSION["ndocumento"])) {
+    $ndocumento = $_SESSION["ndocumento"];
+
+    // Consulta segura
+    $stmt = mysqli_prepare($enlace, "SELECT * FROM usuario WHERE ndocumento = ?");
+    mysqli_stmt_bind_param($stmt, "s", $ndocumento);
+    mysqli_stmt_execute($stmt);
+    $resultado = mysqli_stmt_get_result($stmt);
+
+    $usuario = mysqli_fetch_assoc($resultado);
+}
+?>
 <!DOCTYPE html>
 <html lang="es">
 
