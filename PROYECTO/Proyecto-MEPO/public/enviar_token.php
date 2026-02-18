29ba417d5eb244041fae8ebf549be67e44b50f90
Josmanpro <josephalfonsoforero@gmail.com>
2026-02-18 09:00:36 -0500

    Actualización GITHUB

--- PROYECTO/Proyecto-MEPO/public/enviar_token.php
+++ PROYECTO/Proyecto-MEPO/public/enviar_token.php
@@ -1,26 +1,38 @@
 <?php
-include("../bdd/conexion.php");
 
-$correo = $_POST['user'];
+$servidor = "localhost";
+$usuario = "root";
+$clave = "";
+$basededatos = "mepo";
 
-$sql = "SELECT * FROM usuario WHERE correo='$correo'";
-$resultado = mysqli_query($conexion, $sql);
+$enlace = mysqli_connect($servidor,$usuario,$clave,$basededatos);
 
-if(mysqli_num_rows($resultado) == 1){
+if(isset($_POST['recoverEmail'])){
 
-    $token = bin2hex(random_bytes(32));
-    $expira = date("Y-m-d H:i:s", strtotime("+1 hour"));
+    $correo_tel = mysqli_real_escape_string($enlace, $_POST['recoverEmail']);
 
-    $update = "UPDATE usuario 
-               SET token='$token', token_expira='$expira' 
-               WHERE correo='$correo'";
+    $sql = "SELECT * FROM usuario WHERE correo_tel='$correo_tel'";
+    $resultado = mysqli_query($enlace, $sql);
 
-    mysqli_query($conexion, $update);
+    if(mysqli_num_rows($resultado) > 0){
 
-    echo "Enlace generado:<br>";
-    echo "<a href='cambiar_contrasena.php?token=$token'>Cambiar contraseña</a>";
+        $token = bin2hex(random_bytes(16));
+        $expira = date("Y-m-d H:i:s", strtotime("+1 hour"));
 
-}else{
-    echo "Correo no registrado";
+        $update = "UPDATE usuario 
+                   SET token='$token', token_expira='$expira'
+                   WHERE correo_tel='$correo_tel'";
+
+        mysqli_query($enlace, $update);
+
+        // 🔥 Simulación de envío (como estás en local)
+        echo "Enlace de recuperación:<br>";
+        echo "<a href='guardar_nueva.php?token=$token'>
+                Cambiar contraseña
+              </a>";
+
+    } else {
+        echo "El correo no existe";
+    }
 }
 ?>
