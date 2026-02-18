7adcfe73d99aeeb16b52b6769f239a3f7a1db5cf
sara <vanegasnicoll005@gmail.com>
2026-02-18 10:32:41 -0500

    Actualización GITHUB

--- PROYECTO/Proyecto-MEPO/public/login.php
+++ PROYECTO/Proyecto-MEPO/public/login.php
@@ -45,7 +45,7 @@ if(isset($_POST["btn-login-form"])){
         $_SESSION ["ndocumento"] = $fila["ndocumento"];
         $_SESSION ["nombre"] = $fila["nombre"];
 
-        header("Location: index.php");
+        header("Location: ahorro.php");
         exit();
     }else {
         echo "Datos incorrectos o usuario no activo";
