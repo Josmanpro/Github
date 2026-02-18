7f211f32db38967ff5d5e3882ae49b82845b402e
sara <vanegasnicoll005@gmail.com>
2026-02-18 06:42:03 -0500

    Actualización GITHUB

--- PROYECTO/Proyecto-MEPO/public/index.php
+++ PROYECTO/Proyecto-MEPO/public/index.php
@@ -8,7 +8,7 @@ if(isset($_SESSION["ndocumento"])) {
     $ndocumento = $_SESSION["ndocumento"];
 
     // Consulta segura
-    $stmt = mysqli_prepare($conexion, "SELECT * FROM usuarios WHERE ndocumento = ?");
+    $stmt = mysqli_prepare($conexion, "SELECT * FROM usuario WHERE ndocumento = ?");
     mysqli_stmt_bind_param($stmt, "s", $ndocumento);
     mysqli_stmt_execute($stmt);
     $resultado = mysqli_stmt_get_result($stmt);
