0e977ce78c02dd170fe6bdf3898474949f405e88
davidcoy <davidcoytrujillo.9@gmail.com>
2026-02-23 11:21:25 -0500

    Actualización GITHUB

---
+++ PROYECTO/Proyecto-MEPO/public/php/valini.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+require("regis2.php"); // conexión a la base de datos
+
+$usuario = null;
+
+if (isset($_SESSION["ndocumento"])) {
+    $ndocumento = $_SESSION["ndocumento"];
+
+    // Consulta segura
+    $stmt = mysqli_prepare($enlace, "SELECT * FROM usuario WHERE ndocumento = ?");
+    mysqli_stmt_bind_param($stmt, "s", $ndocumento);
+    mysqli_stmt_execute($stmt);
+    $resultado = mysqli_stmt_get_result($stmt);
+
+    $usuario = mysqli_fetch_assoc($resultado);
+}
+?>
