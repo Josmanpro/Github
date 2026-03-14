a64c859659f0e28c7615ab4e58c213dbb1af550b
santiago2 <santiagoamariles08@gmail.com>
2026-03-14 09:48:51 -0500

    Initial commit

--- Proyecto-MEPO/public/php/agregar_producto.php
+++ Proyecto-MEPO/public/php/agregar_producto.php
@@ -31,30 +31,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     }
 }
 ?>
-<!DOCTYPE html>
-<html lang="es">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>MEPO - Mercando con Propósito</title>
-    <link rel="stylesheet" href="../css/pagina.css">
-    <link rel="stylesheet" href="../css/perfil.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet">
-
-</head>
-
-<body>
-
-    <header class="main-header">
-        <div class="container">
-            <a href="index.php" class="logo">Mepo</a>
-            <nav class="main-nav">
-                <a href="comparar.php" class="button">Comparar</a>
-                <a href="supermercados.php" class="button">Supermercados</a>
-                <a href="ofertas.php" class="button">Ofertas</a>
 <style>
     .upload-box{
     width:250px;
