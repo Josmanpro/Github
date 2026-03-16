4b0f1780188667ecf953473adaed3e70cfd6cc8a
davidcoy <davidcoytrujillo.9@gmail.com>
2026-03-16 06:39:06 -0500

    Actualización GITHUB

--- Proyecto-MEPO/public/php/panel_meta.php
+++ Proyecto-MEPO/public/php/panel_meta.php
@@ -82,7 +82,7 @@ if ($porcentaje > 100) {
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet">
->>>>>>> ecc1816558b39d189d30343ec38adafc46fe5dee
+
 
 </head>
 
